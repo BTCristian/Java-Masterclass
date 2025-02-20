e34a8f95c2840a39016ea8d2c016a714153eb14c
BTCristian <tcristian.b@gmail.com>
2025-02-20 07:59:21 +0000

    MB Converter

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,7 @@ public class Main {
         System.out.println(
                 "10 Km/h = " + SpeedConverter.toMilesPerHour(10)
                 + " mi/h");
+
+        MegaBytesConverter.printMegaBytesAndKiloBytes(1879);
     }
 }
