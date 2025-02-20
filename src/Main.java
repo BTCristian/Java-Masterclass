4157f9bbad8f6b6f91874660171a1f096fa21a3f
BTCristian <tcristian.b@gmail.com>
2025-02-20 08:07:25 +0000

    BarkingDog

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java MasterClass Methods Katas");
+        System.out.println("Welcome to Java MasterClass - Katas");
 
+        //PositiveNegativeZero
         PositiveNegativeZero.checkNumber(10);
         PositiveNegativeZero.checkNumber(-5);
         PositiveNegativeZero.checkNumber(0);
 
 
+        //SpeedConverter
         System.out.println(
                 "10 Km/h = " + SpeedConverter.toMilesPerHour(10)
                 + " mi/h");
 
+
+        //MegaBytesConverter
         MegaBytesConverter.printMegaBytesAndKiloBytes(1879);
+
+        System.out.println(BarkingDog.shouldWakeUp(true, 19));
+        System.out.println(BarkingDog.shouldWakeUp(true, 2));
     }
 }
