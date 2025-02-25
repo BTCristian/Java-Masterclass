195f098945f77fc67aabb72203777058ee73cf98
BTCristian <tcristian.b@gmail.com>
2025-02-25 20:19:48 +0000

    PlayingCat

---
+++ src/ControlFlow/SwitchStatement.java
@@ -0,0 +1,29 @@
+package ControlFlow;
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+
+//        int value = 3;
+//
+//        if (value == 1) {
+//            System.out.println("Value was 1");
+//        } else if (value == 2) {
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 3;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3:
+                System.out.println("Was not 1 or 2");
+        }
+    }
+}
